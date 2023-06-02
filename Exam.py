6f591e5cd3befd81a8ac194d6c9064b155789502
shadowpyton <132641686+shadowpyton@users.noreply.github.com>
2023-06-02 11:31:42 +0500

    Exam

---
+++ Exam.py
@@ -0,0 +1,38 @@
+# 2
+# oilam = {"otam":"Alimardon", "onam":"Hurriyatxon", "ukam":"Abdurahimjon",
+#          "singlim":"Muslimaxon", "men":"Azizbek"}
+# 3
+# a = [oilam]
+# print(a)
+# # 4
+# for k,q in oilam.items():
+#     print(f"Kalit: {k}\nQiymat: {q}\n")
+#5
+# print("nesting bu ro'yxat ichida lug'at, lug'at ichida ro'yxat")
+# cars = {
+#     "model":"BWM",
+#     "color":"black",
+#     "price":"500000$",
+# }
+# car = [cars]
+# print(car)
+# 6
+# print("Karra jadvali")
+# son = int(input("Biron-bir son kiriting\n>>>"))
+# s = 1
+# while s<11:
+#     s += 1
+#     print(f"{son}*{s}={son*s}")
+#     if son =='exit':
+#         break
+# 7
+# bunday holatda manavi hatolikni beradi: '<' not supported between instances of 'str' and 'int'
+# Ya'ni 'qiymatga' to'gri vazifa berilmagan misol uchun int, str
+# 8
+# admin = {'Azizbek':'7'}
+# a = input('ismingizni kiriting:')
+# b = input('parolni kiriting:')
+# if a not in admin.keys():
+#     print("Uzr tizimga kirita olmaymiz")
+# else:
+#     print("salom aadmin")
